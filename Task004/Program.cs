2252bc3683ca63cbbd271f9ea58aa1bce7d48ef3
Svetlana Prokopeva <lana.prokopeva@yandex.ru>
2022-09-06 19:38:06 +0300

    Task004

---
+++ Task004/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Определение входных данных
+Console.Write("Введите число от 1 до N: ");
+int number = int.Parse(Console.ReadLine());
+int startNumber = 1;
+// Определяем является ли число четным
+while (startNumber <= number)
+{
+    if ((startNumber%2) == 0)
+    {
+        Console.WriteLine($"{startNumber}");
+    }
+    startNumber ++;
+}    
